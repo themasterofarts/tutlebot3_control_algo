8a4073e985360e45cd23e032e94ac770b00bbcd1
themasterofarts <ulrichnatsou@gmail.com>
2023-04-25 02:47:39 +0200

    adding odometry and path definition funcs

--- multipleshooting mpc for point tracking.py
+++ multipleshooting mpc for point tracking.py
@@ -50,3 +50,38 @@ v_min = -v_max
 omega_max = 2.84                                                
 omega_min = -omega_max
 
+global x,y,theta,qx,qy,qz,qw,V,omega                                        # (x,y,theta) will store the current position and orientation 
+                                                                            # qx,qy,qz,qw will store the quaternions of the bot position
+                                                                            # V and omega will store the inputs to the bot(Speed and Angular Velocity)                                                                                                                                                                                        
+  
+
+global total_path_points                                                                                                                                        
+total_path_points = 0                                                                                                                                                                            
+global path                                                                                                                                         
+
+def odomfunc(odom):
+
+    global x,y,qx,qy,qz,qw,theta
+    x = odom.pose.pose.position.x 
+    y = odom.pose.pose.position.y 
+    qx = odom.pose.pose.orientation.x                                       # quaternions of location
+    qy = odom.pose.pose.orientation.y
+    qz = odom.pose.pose.orientation.z 
+    qw = odom.pose.pose.orientation.w
+
+    theta = math.atan2(2*(qx*qy+qw*qz),1-2*(qy*qy+qz*qz))                   # finding yaw from quaternions
+
+
+
+def pathfunc(Path):                                                                                                                                       
+
+    global total_path_points,path
+    if total_path_points == 0:        
+        total_path_points = len(Path.poses)
+        path = np.zeros((total_path_points,2))													
+        
+    for i in range(0,total_path_points):                                                                                                                
+        path[i][0] = Path.poses[i].pose.position.x
+        path[i][1] = Path.poses[i].pose.position.y   
+
+    
